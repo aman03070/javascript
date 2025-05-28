fef0bcf57e2aa4d26bc827e9cc957927b0fa150a
Aman Jaiswal <125671929+aman03070@users.noreply.github.com>
2025-05-28 10:32:24 +0530

    Add files via upload

---
+++ project2-bmi calculator/chaiaurcode.js
@@ -0,0 +1,15 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const results = document.querySelector('#results');
+
+
+   const bmi = (weight/ ((height*height)/10000)).toFixed(2);
+
+    results.innerHTML = `<span>${bmi}</span>`;
+
+});
